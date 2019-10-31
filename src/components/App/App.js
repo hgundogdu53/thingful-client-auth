ff133b284faf7fa4a5b802229debf025cfaa0886
Huseyin Gundogdu <hgundogdu53@gmail.com>
2019-10-31 16:47:19 -0700

    auth added

---
+++ src/components/App/App.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Header from '../Header/Header'
+import PrivateRoute from '../Utils/PrivateRoute'
+import PublicOnlyRoute from '../Utils/PublicOnlyRoute'
+import ThingListPage from '../../routes/ThingListPage/ThingListPage'
+import ThingPage from '../../routes/ThingPage/ThingPage'
+import LoginPage from '../../routes/LoginPage/LoginPage'
+import RegistrationPage from '../../routes/RegistrationPage/RegistrationPage'
+import NotFoundPage from '../../routes/NotFoundPage/NotFoundPage'
+import TokenService from '../../services/token-service'
+import AuthApiService from '../../services/auth-api-service'
+import IdleService from '../../services/idle-service'
+import './App.css'
+
+class App extends Component {
+  state = { hasError: false }
+
+  static getDerivedStateFromError(error) {
+    console.error(error)
+    return { hasError: true }
+  }
+
+  // =============================================================================
+  // https://github.com/Thinkful-Ed/blogful-client/compare/user-registration...expiry-time-and-refresh
+  // =============================================================================
+
+  componentDidMount() {
+    IdleService.setIdleCallback(this.logoutFromIdle)
+
+    if (TokenService.hasAuthToken()) {
+      IdleService.registerIdleTimerResets()
+      TokenService.queueCallbackBeforeExpiry(() => {
+        AuthApiService.postRefreshToken()
+      })
+    }
+  }
+
+  componentWillUnmount() {
+    IdleService.unRegisterIdleResets()
+    TokenService.clearCallbackBeforeExpiry()
+  }
+
+  logoutFromIdle = () => {
+    TokenService.clearAuthToken()
+    TokenService.clearCallbackBeforeExpiry()
+    IdleService.unRegisterIdleResets()
+    this.forceUpdate()
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App__header'>
+          <Header />
+        </header>
+        <main className='App__main'>
+          {this.state.hasError && <p className='red'>There was an error! Oh no!</p>}
+          <Switch>
+            <Route
+              exact
+              path={'/'}
+              component={ThingListPage}
+            />
+            <PublicOnlyRoute
+              path={'/login'}
+              component={LoginPage}
+            />
+            <PublicOnlyRoute
+              path={'/register'}
+              component={RegistrationPage}
+            />
+            <PrivateRoute
+              path={'/thing/:thingId'}
+              component={ThingPage}
+            />
+            <Route
+              component={NotFoundPage}
+            />
+          </Switch>
+        </main>
+      </div>
+    )
+  }
+}
+
+export default App
